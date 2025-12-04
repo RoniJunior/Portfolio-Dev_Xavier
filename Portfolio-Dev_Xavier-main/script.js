47bee78e5a808597a4ab3eb696a9f2f5b65445a4
Roni Júnior <97416780+RoniJunior@users.noreply.github.com>
2025-12-04 17:08:20 -0300

    Add files via upload

---
+++ Portfolio-Dev_Xavier-main/script.js
@@ -0,0 +1,37 @@
+const words = [
+  "_PostgreSQL",
+  "_Next.JS",
+  "_N8N",
+  "_Node.JS",
+  "_JavaScript",
+];
+let index = 0;
+let charIndex = 0;
+const typingSpeed = 100; // Velocidade de digitação (ms)
+const erasingSpeed = 50; // Velocidade de apagar (ms)
+const delayBetweenWords = 1000; // Intervalo entre palavras (ms)
+const dynamicText = document.querySelector(".dynamic-text");
+
+function typeWord() {
+  if (charIndex < words[index].length) {
+    dynamicText.textContent += words[index][charIndex];
+    charIndex++;
+    setTimeout(typeWord, typingSpeed);
+  } else {
+    setTimeout(eraseWord, delayBetweenWords);
+  }
+}
+
+function eraseWord() {
+  if (charIndex > 0) {
+    dynamicText.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseWord, erasingSpeed);
+  } else {
+    index = (index + 1) % words.length; // Troca para a próxima palavra
+    setTimeout(typeWord, typingSpeed);
+  }
+}
+
+// Inicia a animação
+typeWord();
